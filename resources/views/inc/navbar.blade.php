f708b107fdfd39600fa76bfa075217e1fe565d35
Anik <anik880@gmail.com>
2017-08-15 16:20:44 +0600

    File changes.

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,27 +1,3 @@
-{{--<nav class="navbar navbar-inverse">--}}
-    {{--<div class="container">--}}
-        {{--<div class="navbar-header">--}}
-            {{--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">--}}
-                {{--<span class="sr-only">Toggle navigation</span>--}}
-                {{--<span class="icon-bar"></span>--}}
-                {{--<span class="icon-bar"></span>--}}
-                {{--<span class="icon-bar"></span>--}}
-            {{--</button>--}}
-            {{--<a class="navbar-brand" href="#">Project name</a>--}}
-        {{--</div>--}}
-        {{--<div id="navbar" class="collapse navbar-collapse">--}}
-            {{--<ul class="nav navbar-nav">--}}
-                {{--<li><a href="/">Home</a></li>--}}
-                {{--<li><a href="/about">About</a></li>--}}
-                {{--<li><a href="/services">Services</a></li>--}}
-                {{--<li><a href="/posts">Blog Posts</a></li>--}}
-            {{--</ul>--}}
-            {{--<ul class="nav navbar-nav navbar-right">--}}
-                {{----}}
-            {{--</ul>--}}
-        {{--</div><!--/.nav-collapse -->--}}
-    {{--</div>--}}
-{{--</nav>--}}
 
 {{--<li><a href="/posts/create">Create Post</a></li>--}}
 
@@ -69,6 +45,7 @@
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
+                            <li><a href="/dashboard">Dashboard</a></li>
                             <li>
                                 <a href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
