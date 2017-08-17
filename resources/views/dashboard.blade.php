0495f4aa905aded8fa1f78c06f12cdc4a29ffd8f
Anik <anik880@gmail.com>
2017-08-18 00:05:02 +0600

    New functionality added.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,29 @@
                 <div class="panel-body">
                     <a class="btn btn-primary" href="/posts/create">Create Post</a>
                     <h3>Your All Blog Posts</h3>
+                    @if(count($posts) > 0)
+                        <table class="table table-responsive table-striped">
+                            <tr>
+                                <th>Title</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            @foreach($posts as $post)
+                                <tr>
+                                    <td>{{$post->title}}</td>
+                                    <td><a class="btn btn-default" href="/posts/{{$post->id}}/edit">Edit</a></td>
+                                    <td>
+                                        {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
+                                            {{ Form::hidden('_method', 'DELETE') }}
+                                            {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
+                                        {!! Form::close() !!}
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    @else
+                      <p>You have no posts. Create a new one.</p>
+                    @endif
                 </div>
             </div>
         </div>
